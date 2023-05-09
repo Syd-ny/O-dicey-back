74802ef39cbc9044f2ec85687694c9cb3f0ee00a
NLemireMery <nicolas.lemire.mery@gmail.com>
2023-05-09 11:37:35 +0200

    Add roles to User + fixtures

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -64,6 +64,11 @@ class User
      */
     private $gameUsers;
 
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
     public function __construct()
     {
         $this->gamesDM = new ArrayCollection();
@@ -234,6 +239,22 @@ class User
                 $gameUser->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+        
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
